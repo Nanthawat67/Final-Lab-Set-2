9ca998ba582c27e52b91f98fc75257e7bd18045a
67543210034-4 <a67543210034-4@admins-iMac.local>
2026-03-18 16:23:11 +0700

    Initial commit

--- final-lab-sec2-set2-alice-bob/auth-service/test-db.js
+++
@@ -1,6 +0,0 @@
-const { pool } = require('./db/db');
-
-pool.query('SELECT 1')
-  .then(() => console.log('✅ DB connected!'))
-  .catch(err => console.error('❌ DB error', err.message))
-  .finally(() => pool.end());
